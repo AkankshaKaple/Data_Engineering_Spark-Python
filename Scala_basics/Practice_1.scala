011b869138bc52d1ca25c95e09a47607e269a021
AkankshaKaple <akankshakaple@gmail.com>
2019-07-13 17:49:00 +0530

    Hello Scala Program

---
+++ Scala_basics/Practice_1.scala
@@ -0,0 +1,18 @@
+object Practice_1{
+  println("Hello World")
+
+  def scalaExample(x:Int): Unit = {
+
+    System.out.println("Hello Scala")
+  }
+
+  def main(args: Array[String]): Unit ={
+    scalaExample(3)
+
+  }
+//  println(1+2)
+  System.out.println(Practice_1.main(Array(" ")))
+
+}
+
+//scalaExample
